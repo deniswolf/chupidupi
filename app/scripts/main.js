a9ba4ee71370cb3d0a2abb2ff3d8646f30329c6f
Denis Wolf <denisw@wix.com>
2013-05-29 16:39:08 +0300

    css animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,6 +14,16 @@ require.config({
 require(['app', 'jquery', 'bootstrap'], function (app, $) {
     'use strict';
     // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+//    console.log(app);
+//    console.log('Running jQuery %s', $().jquery);
+    $('document').ready(function(){
+        setTimeout(function () {
+            $('#chupi').removeClass().addClass('animated bounceIn');
+        }, 300);
+
+        setTimeout(function () {
+            $('#dupi').removeClass().addClass('animated lightSpeedIn');
+        }, 1300);
+
+    });
 });
